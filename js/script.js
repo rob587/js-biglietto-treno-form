df5a6ec74248408a009324d9553304ff4efa443e
rob587 <robertocamm02@yahoo.com>
2025-07-10 16:26:06 +0200

    aggiunte informazioni del biglietto

--- js/script.js
+++ js/script.js
@@ -1,27 +1,12 @@
 //su 100km sono 21euro - 20% 16.80 per i minorenni//
 //su 100km sono 21euro - 40% 12.60 per over 65//
 
-const travelRequest =parseInt(prompt("Quanti km vuoi percorrere?"))
-const ageRequest =parseInt(prompt("Quanti anni hai?"))
-
-const yearMinor=18 
-const yearOlder=65
-const ticket= 0.21
-
-let price = travelRequest * 0.21;
-
-if(ageRequest<yearMinor){
-    price= price*0.8
-    
-}
-
-else if(ageRequest>yearOlder){
-    price= price*0.6
-}
 
 
 
-console.log("Prezzo finale: € " + price.toFixed(2));
+
+
+
 
 //-----------//
 
@@ -29,4 +14,32 @@ const button = document.getElementById('btn');
 
 button.addEventListener('click', (e) => {
     e.preventDefault()
+
+
+    const name = document.getElementById('name').value
+    const kmrun = document.getElementById('travel').value
+    const years = document.getElementById('age').value
+
+    const yearMinor= 'minor'
+    const yearOlder= 'old'
+    const ticket= 0.21
+    const km = parseFloat(kmrun)
+
+    let price = km * 0.21;
+
+    if(years===yearMinor){
+    price= price*0.8
+    
+    }
+
+    else if(years===yearOlder){
+    price= price*0.6
+    }
+
+
+
+          //da aggiustare
+
+    document.getElementById('show-price').innerText=   `Ciao ${name}, prezzo: € ${price.toFixed(2)}`;
+
 })
